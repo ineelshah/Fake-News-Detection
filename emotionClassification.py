f54576f6fd3180ea575abe64784b896ee0de569f
Viraj <desaiviraj28@gmail.com>
2020-03-24 18:16:58 +0530

    Accuracy determined

--- emotionClassification.py
+++ emotionClassification.py
@@ -6,11 +6,13 @@ Created on Wed Mar  4 12:35:27 2020
 @author: virajdesai
 """
 
+
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
 from textblob.classifiers import NaiveBayesClassifier
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 
 class EmotionClassify:
@@ -37,7 +39,11 @@ class EmotionClassify:
 
         ## create the training set
         self.train = []
-
+        
+        ##create a test set
+        self.test = []
+        
+        self.trainSplit = 0.9
          # stores the summarized text in a list
         self.sum_text_list = []
 
@@ -51,7 +57,7 @@ class EmotionClassify:
     A function for cleaning up all the documents
     # removes stop words
     # removes punctuations
-    # uses lemmatizer 
+    # uses lemmatizer
     '''
 
     def clean(self, doc):
@@ -83,15 +89,28 @@ class EmotionClassify:
     def iteratePopEmotion(self):
         for i in range(self.new_df.shape[0]):
             self.em_list.append(self.new_df.loc[i]['Emotion'])
-
+        
     '''
     Function to create training set
     '''
-
+        
     def createTrain(self):
-        for i in range(self.new_df.shape[0]):
+        trainLength = (self.new_df.shape[0])*self.trainSplit
+        for i in range(trainLength):
             self.train.append([self.text_list[i], self.em_list[i]])
-
+        #print(len(self.train))
+        
+    
+    '''
+    Function to create test set
+    '''
+    
+    def createTest(self):
+        testLength = (self.new_df.shape[0])*(1-self.trainSplit)
+        for i in range(testLength, self.new_df.shape[0]):
+            self.test.append([self.text_list[i], self.em_list[i]])
+        #print(len(self.test))
+        
     '''
     Function to create model
     classify the query text
@@ -103,36 +122,43 @@ class EmotionClassify:
         cl = NaiveBayesClassifier(self.train)
         return cl
     
-    def classifyText(self, cl, content):
-        result = cl.prob_classify(content)
-        for label in result.samples():
-            print("%s: %f" % (label, result.prob(label))) 
-
+    def performance(self,cl):
+        predictions = []
+        for i in range(len(self.test)):
+            predictions.append(cl.classify(self.test[i][0]))
+        actuals = []
+        for j in range(len(self.test)):
+            actuals.append(self.test[j][1])
+        
+        #print(len(predictions))
+        #print(len(actuals))
+        #print(nltk.classify.accuracy(predictions, actuals)*100)
+        print("Accuracy :" accuracy_score(predictions, actuals)*100)
+        
+    
     '''
     A function which is the driver for the entire class
 
     '''
-
     def main(self):
-        content = "I love reading"
         self.iterateClean()
         self.iteratePopEmotion()
         self.iteratePopText()
         self.createTrain()
-        cl = self.classifyTextTrain()
-        self.classifyText(self, cl, content)
-  
+        self.createTest()
+        cc = self.classifyTextTrain()
+        self.performance(cc)
 
+#method for predicting class for test data
 def classifyText(cl, content):
      result = cl.prob_classify(content)
      for label in result.samples():
-            print("%s: %f" % (label, result.prob(label)))     
+            print("%s: %f" % (label, result.prob(label)))
+        
 
 obj = EmotionClassify()
-cl = obj.classifyTextTrain()
+cl = obj.classifyTextTrain()  # trained Naive Bayes Classifier
 
 classifyText(cl, "I love reading!")
 
-#This is only inital draft not the final product
-#Performance metrics need to be added
-#Cross validation and test dataset partitioning required
+#Due to an accuracy of approximately 50% we need a better classifier.
