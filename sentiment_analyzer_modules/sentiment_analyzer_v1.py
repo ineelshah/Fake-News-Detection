dc60776c0975ccf30be99792e6c3381c65f52df3
Jinit Jain <jinitmjain15698@gmail.com>
2020-04-13 20:14:02 +0530

    sentiment analyzer modules added

---
+++ sentiment_analyzer_modules/sentiment_analyzer_v1.py
@@ -0,0 +1,22 @@
+# import SentimentIntensityAnalyzer class 
+# from vaderSentiment.vaderSentiment module. 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
+
+# function to print sentiments 
+# of the sentence. 
+def sentiment_scores(sentence): 
+    # Create a SentimentIntensityAnalyzer object. 
+    sid_obj = SentimentIntensityAnalyzer()
+    # polarity_scores method of SentimentIntensityAnalyzer 
+    # oject gives a sentiment dictionary. 
+    # which contains pos, neg, neu, and compound scores. 
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    return sentiment_dict
+
+	
+# Driver code 
+if __name__ == "__main__" : 
+    f = open('sentiment_test.txt','r')
+    sentence = f.read()
+    f.close()
+    print(sentiment_scores(sentence))
